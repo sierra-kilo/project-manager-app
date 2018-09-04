f95c3f53c34316f6674c90e17b84592bbcf0b883
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-09-04 16:39:25 -0700

    added projects component, and porjectitem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import Projects from './components/Projects'
 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    };
+  }
+
+  // ajax call here
+  componentDidMount() {
+    this.setState({
+      projects: [
+
+          {
+            title: 'Business Website',
+            category: 'Web Design'
+          },
+          {
+            title: 'Social App',
+            category: 'Mobile Development'
+          },
+          {
+            title: 'Ecommerce Shopping Cart',
+            category: 'Web Development'
+          }
+
+      ]
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         My App
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
